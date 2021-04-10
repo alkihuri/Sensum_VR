6c7fb8a2b8b185205488d93cb4ab8744e6c5e30a
Abdulkadir Karagishiev <kadyrkaragishiev@gmail.com>
2021-04-10 13:58:46 +0300

    Add TextMeshPro, Language Canvas

---
+++ Sensum.Unity/Assets/Scripts/GameController/CanvasButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class CanvasButtonScript : MonoBehaviour
+{
+    public void OnHover()
+    {
+        DOTween.To(()=> GetComponent<Image>().color, x=> GetComponent<Image>().color = x, Color.white, 1);
+    }
+
+    public void OnNotHover()
+    {
+        DOTween.To(()=> GetComponent<Image>().color, x=> GetComponent<Image>().color = x, new Color(0.56f,0.56f,0.56f,1), 1);
+    }
+}
---
+++ Sensum.Unity/Assets/Scripts/GameController/CanvasButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Rendering.PostProcessing;
+public class CanvasButtons : MonoBehaviour
+{
+    private List<string> langStr = new List<string>{"kz","ru","en"};
+    public PostProcessVolume volume;
+    Vignette _vignette;
+    private void Start() {
+        volume.profile.TryGetSettings(out _vignette);    
+    }
+    public static void OpenScene(){
+    }
+    private IEnumerator ShowHide(){
+        DOTween.To(()=> _vignette.opacity.value, x=> _vignette.opacity.value= x, 1f, 1);
+        yield return new WaitForSeconds(1f);
+        DOTween.To(()=> _vignette.opacity.value, x=> _vignette.opacity.value= x, 0f, 1);
+        this.gameObject.SetActive(false);
+    }
+}
